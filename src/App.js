752432c1e49365a8c71689aeabb7b7e17b963e61
Qingquan Li <liqqisme@gmail.com>
2022-11-08 22:23:47 -0500

    Add create-function and read-function of CRUD

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
+import AddTodo from "./components/AddTodo";
+import TaskList from "./components/TaskList";
+
+let nextId = 0;
+
 export default function App() {
+  // Set the local variable nextId=0 here,
+  // the `id` in `setTodos()` will always be zero.
+  // let nextId = 0;
+  const [todos, setTodos] = useState([]);
+
+  // function handleAddTodo(title) {
+  const handleAddTodo = (title) => {
+    // Replace the state with a new array
+    setTodos([
+      // Create a new array which contains the
+      // existing items and a new item at the end.
+      // ... is the object spread syntax used to create a copy of an object.
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+      }
+    ]);
+  }
+
   return (
     <>
-      React Todo App
+      <AddTodo onAddTodo={handleAddTodo}/>
+      <TaskList todos={todos} />
     </>
   );
 }
